642b31a316c95aac34141e8399c4a71e6cd9b145
fsyang <fsyang05@gmail.com>
2026-03-05 16:55:34 -0600

    fixing treesitter

--- init.lua
+++ init.lua
@@ -971,6 +971,9 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter').setup()
+    end,
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
