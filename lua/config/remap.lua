b96565a4af31a87eb5f95bb011c4f72bfb400dc0
fsyang <fsyang05@gmail.com>
2026-05-29 00:08:56 -0500

    move code block

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
         local opts = { buffer = args.buf }
