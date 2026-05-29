04befa27cbdaca834131449fab78d80a643db513
fsyang <fsyang05@gmail.com>
2026-05-29 00:19:32 -0500

    2 spaces for cpp files

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,6 +9,16 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.smartindent = true
 
+-- Lua (init.lua)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cpp",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
 -- search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
